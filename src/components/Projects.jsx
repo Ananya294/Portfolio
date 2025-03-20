88334ac50c8872281ce34bdbb5fd2dd53ed32729
Ananya294 <singhananyaas294@gmail.com>
2025-03-21 02:44:17 +0530

    fixed project card for mobile

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,39 +6,30 @@ import { SectionWrapper } from '../hoc';
 import { styles } from '../styles';
 import { textVariant } from '../utils/motion';
 
-const ProjectCard = ({ project, onClick, isActive, isMobile }) => {
+const ProjectCard = ({ project, onClick, isActive }) => {
   return (
     <div
       onClick={onClick}
-      className={`cursor-pointer sm:mb-5 p-5 max-w-xl relative sm:text-left text-center ${
-        isMobile ? "text-quaternary" : ""
-      }`}
+      className={`cursor-pointer sm:mb-5 p-5 max-w-xl relative sm:text-left text-center`}
     >
-      {(isActive || isMobile) && (
+      {isActive && (
         <div className="absolute left-0 top-0 bottom-0 w-3 md:w-5 bg-tertiary my-6 sm:block hidden"></div>
       )}
       <h3
         className={`text-xl lg:text-2xl xl:text-3xl font-bold sm:pl-8 ${
-          isActive || isMobile ? "text-quaternary" : "text-slate-600"
+          isActive ? "text-quaternary" : "text-slate-600"
         }`}
       >
         {project.title}
       </h3>
-      {/* <p
-        className={`text-md lg:text-lg xl:text-2xl sm:font-medium pt-2 sm:pl-8 ${
-          isActive || isMobile ? "text-white" : "text-slate-600"
-        }`}
-      >
-        {project.date}
-      </p> */}
     </div>
   );
 };
 
-const ProjectDetails =({ project }) => {
+const ProjectDetails = ({ project }) => {
   return (
-    <div className="mt-5">
-      <ul className="max-w-7xl list-none space-y-8 border-4 lg:border-8 rounded-xl lg:rounded-3xl p-6">
+    <div className="mt-5 w-full">
+      <ul className="max-w-7xl list-none space-y-4 border-4 lg:border-8 rounded-xl lg:rounded-3xl p-6">
         {project.details.map((detail, index) => (
           <li
             key={`project-detail-${index}`}
@@ -52,8 +43,8 @@ const ProjectDetails =({ project }) => {
 };
 
 const Projects = () => {
-  const [selectedProject, setSelectedProject] = useState(projects[0]);
-  const [isMobile, setIsMobile] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 640);
 
   useEffect(() => {
     const handleResize = () => {
@@ -71,9 +62,7 @@ const Projects = () => {
   return (
     <div className="sm:my-20">
       <motion.div variants={textVariant()}>
-        <h2 className={`${styles.sectionText} text-center`}>
-          Projects
-        </h2>
+        <h2 className={`${styles.sectionText} text-center`}>Projects</h2>
       </motion.div>
 
       <div className="relative mt-10 md:mt-20 md:p-20 flex flex-col items-center sm:flex-row sm:items-start">
@@ -84,14 +73,16 @@ const Projects = () => {
               project={project}
               onClick={() => setSelectedProject(project)}
               isActive={selectedProject === project}
-              isMobile={isMobile}
             />
           ))}
         </div>
 
-        <div className="flex justify-end z-10 sm:block hidden">
-          <ProjectDetails project={selectedProject} />
-        </div>
+        {/* Show Project Details only when a project is selected */}
+        {selectedProject && (
+          <div className={`flex justify-center z-10 w-full ${isMobile ? "mt-5" : ""}`}>
+            <ProjectDetails project={selectedProject} />
+          </div>
+        )}
       </div>
     </div>
   );
